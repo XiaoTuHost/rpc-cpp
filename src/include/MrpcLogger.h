b681ea934c644d213b330063e1ace8cfbe670176
xthost <mr107xt@163.com>
2025-02-27 11:01:48 +0800

    添加客户端头文件

---
+++ src/include/MrpcLogger.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef __MRPCLOGGER__H__
+#define __MRPCLOGGER__H__
+
+
+
+#endif // !__MRPCLOGGER__H__
---
+++ src/include/Mrpcchannel.h
@@ -0,0 +1,46 @@
+#pragma once
+#ifndef __MRPCCHANNEL__H__
+#define __MRPCCHANNEL__H__
+
+#include <google/protobuf/service.h>
+#include <zookeeperutil.h>
+
+class Mrpcchannel : public google::protobuf::RpcChannel
+{
+    public:
+        Mrpcchannel(bool connectNow);
+        virtual ~Mrpcchannel()
+        {
+        }
+        /*
+        virtual void CallMethod(
+            const MethodDescriptor* method,      // 被调用的方法描述符（自动生成）
+            RpcController* controller,           // 控制调用的上下文（如错误处理）
+            const Message* request,              // 请求消息（序列化后发送）
+            Message* response,                   // 响应消息（反序列化后填充）
+            Closure* done                        // 异步回调（可选）
+        ) = 0;
+         */
+        void CallMethod(const ::google::protobuf::MethodDescriptor* method,
+                        ::google::protobuf::RpcController* controller,
+                        const ::google::protobuf::Message* request,
+                        ::google::protobuf::Message* response,
+                        ::google::protobuf::Closure* done
+                    ) override;
+        
+    private:
+        int m_client;
+        std::string service_name;
+        std::string m_ip;
+        uint16_t m_port;
+        std::string method_name;
+        int m_index;
+        
+        bool newConnect(const char* ip,uint16_t port);
+        std::string QueryServiceHost(ZkClient* zkClient,
+            std::string service_name,
+            std::string method_name,
+            int &idx);
+};
+
+#endif // !__MRPCCHANNEL__H__
